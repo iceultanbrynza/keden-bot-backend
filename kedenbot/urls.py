e181e34ddb6c09824da2ebbc15acf61d3ee3baea
Aisultan <aysultan2005@gmail.com>
2025-06-21 13:25:47 +0500

    fix: screen and video loadingv2

--- kedenbot/urls.py
+++ kedenbot/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.http import HttpResponse
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path("", lambda r: HttpResponse("Root is alive")),
     path('kedenbot/', include('kedenwebpages.urls')),
 ]
